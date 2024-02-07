04cc9a8e2da922568656dacbb2d01a605c09ff6e
Nada Tandaric <nadalina.t5@gmail.com>
2024-02-07 21:44:32 +0100

    application properties

--- demo-spring5/src/main/java/com/example/spring/controllers/Controller.java
+++ demo-spring5/src/main/java/com/example/spring/controllers/Controller.java
@@ -1,13 +1,17 @@
 package com.example.spring.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 	
+	@Value("${app.name}")
+	private String appName;
+	
 	@RequestMapping("/hello")
 	public String helo() {
-		return "Hello";
+		return "Hello " + appName;
 	}
 }
